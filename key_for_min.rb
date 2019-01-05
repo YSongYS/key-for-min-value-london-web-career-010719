09cc956bf3e65673268d15256eee542eee842622
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 15:20:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,9 @@ def key_for_min_value(name_hash)
   min_value = 1.0/0.0
   name_hash.each do |item, value|
     if value < min_value
-      puts value
-      puts min_value
       min_key = item
-      puts min_key
+      min_value = value
     end
   end
   min_key
 end
-
-thehash = {:blake => 10, :ashley => 50, :adam => 17}
-
-key_for_min_value(thehash)
